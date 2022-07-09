14c42ad3b272f3ff1efe1eaeb0dfda7183f3ab00
Natalia <gurova_nata22@mail.ru>
2022-07-09 22:57:30 +0300

    Add Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else  
+{
+    if(number1 > number2)
+    {
+        Console.WriteLine("Первое число " + number1 + " большее, второе число " + number2 + " меньшее.");
+    }
+    else
+    {
+         Console.WriteLine("Первое число " + number1 + " меньшее, второе число " + number2 + " большее.");
+    }
+}
