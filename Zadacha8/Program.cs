72d1f82c6a5bd45c2adc65908b516da35db69d6f
Natalia <gurova_nata22@mail.ru>
2022-07-10 01:20:27 +0300

    Add Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое неотрицательное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int start = 2;
+
+if(number <= 1)
+{
+    Console.Write("Введены некоректные данные!!! Перезапустите программу и повторите ввод вновь!");
+}
+else
+{
+    while (start <= number)
+    {
+        Console.Write(start + " ");
+        start = start + 2;
+    }
+}
+Console.WriteLine();
